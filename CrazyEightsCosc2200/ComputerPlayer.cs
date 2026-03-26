0bec6c93fb13e5317aed3a2a1a03bca0d87ab32a
Yiwei27 <yiwei.li@dcmail.ca>
2026-03-25 22:01:10 -0400

    Created few classes and add auto update computer player cards

---
+++ CrazyEightsCosc2200/ComputerPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyEightsCosc2200
+{
+    internal class ComputerPlayer : Player
+    {
+        public ComputerPlayer(string name) : base(name)
+        {
+        }
+    }
+}
---
+++ CrazyEightsCosc2200/GameLogic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyEightsCosc2200
+{
+    internal class GameLogic : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ComputerPlayer computerPlayer = new ComputerPlayer("AI");
+
+        public string OpponentCardCountText
+        {
+            get => $"Cards: {computerPlayer.hand.Count}";
+        }
+
+        public void UpdateUI()
+        {
+            OnPropertyChanged(nameof(OpponentCardCountText));
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
